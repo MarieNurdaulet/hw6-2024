9a6b1a06e6394086c4aaa068abe7a3c0702302ce
Marzhan Nurdauletova <60551196+MarieNurdaulet@users.noreply.github.com>
2024-11-16 15:35:45 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -12,6 +12,7 @@ window.addEventListener("load", function() {
 
 document.querySelector("#play").addEventListener("click", function() {
 	video.play();
+	video.volume = 1.0;
 	console.log("Play Video");
 });
 
